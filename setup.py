0e4d5dd5e3389bf0c777dc6249e729a0f7882e4a
Anton Gorinenko <anton.gorinenko@gmail.com>
2026-05-24 07:38:36 +0300

    Добавлена команда проверки REST API хелсчеков

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name='http-misc',
-    version='3.1.4',
+    version='3.1.5',
     author='Anton Gorinenko',
     author_email='anton.gorinenko@gmail.com',
     description='Утилитарный пакет межсервисного взаимодействия по протоколу HTTP',
